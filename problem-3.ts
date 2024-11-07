5bdce6691301c0a85b62fbb84d55ab02aab0e806
faijulislambd <f.faijulislam144@gmail.com>
2024-11-07 21:57:15 +0600

    3rd Problem Completed

--- problem-3.ts
+++ problem-3.ts
@@ -1,2 +1,19 @@
 {
+  // Problem-3 Get the number of word in sentence
+
+  const sentence: string = "I love programming! Programming fun!";
+  const word: string = "programming";
+  const countWordOccurrences = (sentence: string, word: string): number => {
+    const searchWord: string = word.toLocaleLowerCase();
+    const searchSentence: string[] = sentence.toLocaleLowerCase().split(" ");
+    const numberOfSearchWord: number = searchSentence.filter(
+      (word) => word.replace(/[^\w]/g, "") === searchWord
+    ).length;
+    return numberOfSearchWord;
+  };
+
+  console.log(
+    `The number of instance of "${word}" in "${sentence}" is`,
+    countWordOccurrences(sentence, word)
+  );
 }
