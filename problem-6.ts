f4f47e414729ac4d0254ac345d37854ec9361117
faijulislambd <f.faijulislam144@gmail.com>
2024-11-08 01:06:56 +0600

    6th Problem Done

---
+++ problem-6.ts
@@ -0,0 +1,25 @@
+{
+  // Problem-6 Get the number of word in sentence
+  // Declaring Generic Type
+  type KeyOf<T> = keyof T;
+
+  let person: { name: string; age: number; email: string } = {
+    name: "Fuad",
+    age: 29,
+    email: "f.faijulislam144@gmail.com",
+  };
+
+  const updateProfile = <T extends object, K extends Partial<T>>(
+    person: T,
+    updateValue: K
+  ) => {
+    for (const key in updateValue) {
+      if (key in person) {
+        (person as any)[key] = updateValue[key];
+      }
+    }
+    return person;
+  };
+
+  console.log(updateProfile(person, { age: 30 }));
+}
